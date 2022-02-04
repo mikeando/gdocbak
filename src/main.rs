c49523aa1928befe66c8b2cec6b723edf297d600
Michael Anderson <drmikeando@gmail.com>
2022-02-04 21:23:25 +0800

    #3 only download new/changed files.
    Determines whether files have changes using the
    last-changed attribute reported by
    gdrive which we then track in a json file.
    
    Fixes #3

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,97 @@
 extern crate google_drive3 as drive3;
 
 use std::{
+    collections::BTreeMap,
     convert::{TryFrom, TryInto},
-    io::Write,
+    io::{BufReader, Write},
 };
 
 use drive3::{oauth2, DriveHub};
 use hyper::body::to_bytes;
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
+enum State {
+    Downloaded,
+    TooLarge,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FileMapEntry {
+    name: String,
+    modified_time: String,
+    state: State,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FileMap {
+    entries: BTreeMap<String, FileMapEntry>,
+}
+
+impl FileMap {
+    pub(crate) fn needs_download(&self, f: &File) -> bool {
+        match self.get(&f.id) {
+            Some(entry) => entry.modified_time != f.modified_time,
+            None => true,
+        }
+    }
+
+    pub(crate) fn update(&mut self, f: &File) {
+        if let Some(e) = self.entries.get_mut(&f.id) {
+            e.modified_time = f.modified_time.clone();
+            e.name = f.name.clone();
+            return;
+        }
+        self.entries.insert(
+            f.id.clone(),
+            FileMapEntry {
+                modified_time: f.modified_time.clone(),
+                name: f.name.clone(),
+                state: State::Downloaded,
+            },
+        );
+    }
+
+    fn get(&self, id: &str) -> Option<&FileMapEntry> {
+        self.entries.get(id)
+    }
+
+    pub(crate) fn mark_as_large(&mut self, f: File) {
+        if let Some(e) = self.entries.get_mut(&f.id) {
+            e.modified_time = f.modified_time.clone();
+            e.name = f.name.clone();
+            e.state = State::TooLarge;
+            return;
+        }
+        self.entries.insert(
+            f.id.clone(),
+            FileMapEntry {
+                modified_time: f.modified_time.clone(),
+                name: f.name.clone(),
+                state: State::TooLarge,
+            },
+        );
+    }
+}
+
+#[derive(Debug)]
 struct File {
     id: String,
     name: String,
     mime_type: String,
     owned_by_me: bool,
+    modified_time: String,
+    trashed: bool,
 }
 
 #[derive(Debug)]
 enum ConversionError {
-    BadId,
-    BadName,
-    BadMimeType,
-    BadOwnedByMe,
+    MissingFieldId,
+    MissingFieldName,
+    MissingFieldMimeType,
+    MissingFieldOwnedByMe,
+    MissingFieldModifiedTime,
+    MissingFieldTrashed,
 }
 
 impl TryFrom<&drive3::api::File> for File {
@@ -28,29 +99,49 @@ impl TryFrom<&drive3::api::File> for File {
 
     fn try_from(value: &drive3::api::File) -> Result<Self, Self::Error> {
         Ok(File {
-            id: value.id.clone().ok_or(ConversionError::BadId)?,
-            name: value.name.clone().ok_or(ConversionError::BadName)?,
+            id: value.id.clone().ok_or(ConversionError::MissingFieldId)?,
+            name: value
+                .name
+                .clone()
+                .ok_or(ConversionError::MissingFieldName)?,
             mime_type: value
                 .mime_type
                 .clone()
-                .ok_or(ConversionError::BadMimeType)?,
-            owned_by_me: value.owned_by_me.ok_or(ConversionError::BadOwnedByMe)?,
+                .ok_or(ConversionError::MissingFieldMimeType)?,
+            owned_by_me: value
+                .owned_by_me
+                .ok_or(ConversionError::MissingFieldOwnedByMe)?,
+            modified_time: value
+                .modified_time
+                .clone()
+                .ok_or(ConversionError::MissingFieldModifiedTime)?,
+            trashed: value.trashed.ok_or(ConversionError::MissingFieldTrashed)?,
         })
     }
 }
 
 #[tokio::main]
 async fn main() {
+    // Load information about files that have already been downloaded.
+    // Open the file in read-only mode with buffer.
+    let mut filemap: FileMap = match std::fs::File::open("downloads/filemap.json") {
+        Ok(file) => {
+            let reader = BufReader::new(file);
+            serde_json::from_reader(reader).unwrap()
+        }
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => FileMap {
+            entries: BTreeMap::new(),
+        },
+        Err(e) => {
+            panic!("Unexpected error reading filemap.json: {}", e);
+        }
+    };
+
     let file = std::fs::File::open("./client_id.json").unwrap();
     let console_app_secret: oauth2::ConsoleApplicationSecret =
         serde_json::from_reader(file).unwrap();
     let secret = console_app_secret.installed.unwrap();
 
-    // Instantiate the authenticator. It will choose a suitable authentication flow for you,
-    // unless you replace  `None` with the desired Flow.
-    // Provide your own `AuthenticatorDelegate` to adjust the way it operates and get feedback about
-    // what's going on. You probably want to bring in your own `TokenStorage` to persist tokens and
-    // retrieve them from storage.
     let auth = oauth2::InstalledFlowAuthenticator::builder(
         secret,
         oauth2::InstalledFlowReturnMethod::Interactive,
@@ -67,7 +158,11 @@ async fn main() {
     let r = hub
         .files()
         .list()
-        .param("fields", "files(id,name,mimeType,ownedByMe)")
+        .q("mimeType='application/vnd.google-apps.document'")
+        .param(
+            "fields",
+            "files(id,name,mimeType,ownedByMe,md5Checksum,modifiedTime,size,trashed,version)",
+        )
         .doit()
         .await
         .unwrap()
@@ -75,38 +170,42 @@ async fn main() {
     if r.next_page_token.is_some() {
         panic!("PAGINATION NOT YET IMPLEMENTED!")
     }
+
     let files: Vec<File> = r
         .files
         .unwrap()
         .iter()
         .map(|f| f.try_into().unwrap())
         .collect();
-    for f in &files {
-        println!("{} '{}' {} {}", f.id, f.name, f.mime_type, f.owned_by_me);
-    }
 
     'file_loop: for f in files {
-        if f.mime_type == "application/vnd.google-apps.document" && f.owned_by_me {
+        if f.mime_type == "application/vnd.google-apps.document" && f.owned_by_me && !f.trashed {
+            if !filemap.needs_download(&f) {
+                println!("skipping {} as it is up-to-date", f.name);
+                continue;
+            }
+
             let response = hub
                 .files()
                 .export(&f.id, "application/vnd.oasis.opendocument.text")
                 .doit()
                 .await;
-            if let Err(e) = &response {
-                if let drive3::Error::BadRequest(x) = e {
-                    for e in &x.error.errors {
-                        if e.domain == "global" && e.reason == "exportSizeLimitExceeded" {
-                            eprintln!(
-                                "WARNING: Unable to download '{}' as .odt as it is too large!",
-                                f.name
-                            );
-                            continue 'file_loop;
-                        }
+            if let Err(drive3::Error::BadRequest(x)) = &response {
+                for e in &x.error.errors {
+                    if e.domain == "global" && e.reason == "exportSizeLimitExceeded" {
+                        eprintln!(
+                            "WARNING: Unable to download '{}' as .odt as it is too large!",
+                            f.name
+                        );
+                        filemap.mark_as_large(f);
+                        continue 'file_loop;
                     }
-                } else {
-                    eprintln!("Unexpected error when downloading '{}' - aborting", f.name);
-                    eprintln!("error is\n{:#?}", e);
                 }
+            }
+            if let Err(e) = &response {
+                eprintln!("Unexpected error when downloading '{}' - aborting", f.name);
+                eprintln!("error is\n{:#?}", e);
+                panic!();
             }
 
             let v = response.unwrap();
@@ -120,8 +219,12 @@ async fn main() {
                 out_name,
                 content.len()
             );
-            let mut f = std::fs::File::create(format!("downloads/{}.odt", out_name)).unwrap();
-            f.write_all(&content).unwrap();
+            let mut ff = std::fs::File::create(format!("downloads/{}.odt", out_name)).unwrap();
+            ff.write_all(&content).unwrap();
+            filemap.update(&f);
         }
     }
+
+    let mut filemap_file = std::fs::File::create("downloads/filemap.json").unwrap();
+    serde_json::to_writer_pretty(&mut filemap_file, &filemap).unwrap();
 }
